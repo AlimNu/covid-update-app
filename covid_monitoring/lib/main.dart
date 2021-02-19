78767b63aff902a0c07eeea4533a52467adbf065
AlimNu <noeralim21@gmail.com>
2021-02-19 15:30:45 +0800

    fix overflow pixel on dropdown, add new container contain total death, total confirmed and total recover

--- covid_monitoring/lib/main.dart
+++ covid_monitoring/lib/main.dart
@@ -6,13 +6,18 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Covid App',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
--- covid_monitoring/lib/screens/home/component/body.dart
+++ covid_monitoring/lib/screens/home/component/body.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:covid_monitoring/constant.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:searchable_dropdown/searchable_dropdown.dart';
@@ -44,59 +45,113 @@ class _BodyState extends State<Body> {
         .of(context)
         .size;
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              height: size.height * 0.35,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                height: size.height * 0.35,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: kPrimaryLightColor,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/header.png'),
+                    )),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              height: 50,
               width: double.infinity,
               decoration: BoxDecoration(
-                  color: kPrimaryLightColor,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/header.png'),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: Colors.black,
                   )),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(Icons.location_pin, color: kPrimaryLightColor,),
+                  Expanded(
+                      child: SearchableDropdown.single(
+                        iconSize: 24,
+                        closeButton: 'Close',
+                        value: _mySelection,
+                        hint: Text('Select Country'),
+                        isExpanded: true,
+                        underline: Padding(
+                          padding: EdgeInsets.all(1),
+                        ),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _mySelection = newValue;
+                          });
+                        },
+                        items: _dataCountry.map((country) {
+                          return DropdownMenuItem(
+                            child: new Text(country.country),
+                            value: country.country,
+                          );
+                        }).toList(),
+                        style: TextStyle(color: kPrimaryLightColor),
+                      )),
+                ],
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            height: 60,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(
-                  color: Colors.black,
-                )),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(Icons.location_pin, color: kPrimaryLightColor,),
-                SizedBox(width: 20,),
-                Expanded(
-                    child: SearchableDropdown.single(
-                      iconSize: 24,
-                      value: _mySelection,
-                      hint: Text('Select Country'),
-                      isExpanded: true,
-                      onChanged: (newValue) {
-                        setState(() {
-                          _mySelection = newValue;
-                        });
-                      },
-                      items: _dataCountry.map((country) {
-                        return DropdownMenuItem(
-                          child: new Text(country.country),
-                          value: country.country,
-                        );
-                      }).toList(),
-                      style: TextStyle(color: kPrimaryLightColor),
-                      underline: SizedBox(),
-                    )),
-              ],
+            SizedBox(
+              height: 20,
             ),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              height: 400,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: Colors.transparent,
+                  )),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      Image.asset('assets/images/positif.png', height: 120, width: 100,),
+                      SizedBox(height: 10,),
+                      Text("Total Death", style: TextStyle(fontWeight: FontWeight.bold,color: kPrimaryLightColor),),
+                      SizedBox(height: 5,),
+                      Text("2439639"),
+                    ],
+                  ),
+                  SizedBox(width: 10,),
+                  Column(
+                    children: [
+                      Image.asset('assets/images/positif++.png', height: 120, width: 100,),
+                      SizedBox(height: 10,),
+                      Text("Total Confirmed", style: TextStyle(fontWeight: FontWeight.bold, color: kPrimaryLightColor),),
+                      SizedBox(height: 5,),
+                      Text("110160813"),
+                    ],
+                  ),
+                  SizedBox(width: 10,),
+                  Column(
+                    children: [
+                      Image.asset('assets/images/negatif.png', height: 120, width: 100,),
+                      SizedBox(height: 10,),
+                      Text("Total Recover", style: TextStyle(fontWeight: FontWeight.bold, color: kPrimaryLightColor),),
+                      SizedBox(height: 5,),
+                      Text("62101472")
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
